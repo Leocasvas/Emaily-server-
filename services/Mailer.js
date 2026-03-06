5532489999014aa2a52a1da17a76dfccd66b1ae8
Leocasvas <auqlicp@gmail.com>
2026-03-06 10:24:31 -0600

    disable clicktraking

--- services/Mailer.js
+++ services/Mailer.js
@@ -19,7 +19,7 @@ class Mailer extends helper.Mail {
 
   addClickTracking() {
     const trackingSettings = new helper.TrackingSettings();
-    const clickTracking = new helper.ClickTracking(true, true);
+    const clickTracking = new helper.ClickTracking(false, false);
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
