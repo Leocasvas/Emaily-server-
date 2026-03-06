3875e24999df273532a3e6bde70c6fff6cec245b
Leocasvas <auqlicp@gmail.com>
2026-03-06 13:54:54 -0600

    emial traking fix header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import PaymentsStripe from "./PaymentsStripe";
 
-class Header extends Component {
-  renderContent() {
-    switch (this.props.auth) {
+const Header = () => {
+  const auth = useSelector((state) => state.auth);
+
+  const renderContent = () => {
+    switch (auth) {
       case null:
         return;
       case false:
@@ -16,38 +18,33 @@ class Header extends Component {
         );
       default:
         return [
-          <li key="1">
+          <li key="payments">
             <PaymentsStripe />
           </li>,
-          <li key="3" style={{ margin: "0 10px" }}>
-            Credits: {this.props.auth.credits}
+          <li key="credits" style={{ margin: "0 10px" }}>
+            Credits: {auth.credits}
           </li>,
-          <li key="2">
+          <li key="logout">
             <a href="/api/logout">Logout</a>
           </li>,
         ];
     }
-  }
+  };
 
-  render() {
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link
-            to={this.props.auth ? "/surveys" : "/"}
-            className="left brand-logo"
-          >
-            Emaily
-          </Link>
-          <ul className="right">{this.renderContent()}</ul>
-        </div>
-      </nav>
-    );
-  }
-}
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <Link
+          to={auth ? "/surveys" : "/"}
+          className="left brand-logo"
+          style={{ marginLeft: "10px" }}
+        >
+          Emaily
+        </Link>
+        <ul className="right">{renderContent()}</ul>
+      </div>
+    </nav>
+  );
+};
 
-function mapStateToProps({ auth }) {
-  return { auth };
-}
-
-export default connect(mapStateToProps)(Header);
+export default Header;
